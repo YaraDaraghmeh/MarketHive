b5be2b2652afa6043e991a9a66043989415dbdf6
Yara <yaradaraghmeh056@gmail.com>
2026-04-13 21:16:52 +0300

    Initial commit

---
+++ src/main/java/com/markethive/MarketHive/MarketHiveApplication.java
@@ -0,0 +1,13 @@
+package com.markethive.MarketHive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketHiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketHiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/markethive/MarketHive/MarketHiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.markethive.MarketHive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketHiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
