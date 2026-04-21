275cec93b24d6613fd51a5745218fb3846756b75
Yara <yaradaraghmeh056@gmail.com>
2026-04-21 23:54:02 +0300

    Payment with first 3 fields   Entity

---
+++ src/main/java/com/markethive/MarketHive/entity/Payment.java
@@ -0,0 +1,29 @@
+package com.markethive.MarketHive.entity;
+
+
+import com.markethive.MarketHive.enums.PaymentStatus;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "payments")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+public class Payment {
+
+    @Id
+    @Column(length = 36)
+    private String id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id", nullable = false, unique = true)
+    private Order order;
+
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal amount;
+
+
+}
