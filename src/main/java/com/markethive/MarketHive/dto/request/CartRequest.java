db6b880f0f241712bb0e23193240d3ea10cb32cb
Yara <yaradaraghmeh056@gmail.com>
2026-04-24 23:48:37 +0300

    Class Request fields

--- src/main/java/com/markethive/MarketHive/dto/request/CartRequest.java
+++ src/main/java/com/markethive/MarketHive/dto/request/CartRequest.java
@@ -4,3 +4,11 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
+@Data
+public class CartRequest {
+    @NotBlank
+    private String productId;
+    @Min(1)
+    private int quantity = 1;
+}
+
