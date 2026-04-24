995f802510a72623b8b45d46e04716ee636cff85
Yara <yaradaraghmeh056@gmail.com>
2026-04-24 23:46:58 +0300

    Order Request Class fields

--- src/main/java/com/markethive/MarketHive/dto/request/OrderRequest.java
+++ src/main/java/com/markethive/MarketHive/dto/request/OrderRequest.java
@@ -2,6 +2,13 @@ package com.markethive.MarketHive.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
+@Data
+public class OrderRequest {
+    @NotBlank
+    private String shippingAddress;
 
+
+    // Items come from the user's cart automatically
+}
 
 
