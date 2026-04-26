4b88f0667dba9d68093e487285216398fa6f1cea
Yara <yaradaraghmeh056@gmail.com>
2026-04-26 20:52:07 +0300

    Get All Reviews for a product

---
+++ src/main/java/com/markethive/MarketHive/controller/ReviewController.java
@@ -0,0 +1,34 @@
+package com.markethive.MarketHive.controller;
+
+
+import com.markethive.MarketHive.dto.request.ReviewRequest;
+import com.markethive.MarketHive.dto.response.ApiResponse;
+import com.markethive.MarketHive.dto.response.ReviewResponse;
+import com.markethive.MarketHive.entity.User;
+import com.markethive.MarketHive.services.ReviewService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class ReviewController {
+
+    private final ReviewService reviewService;
+
+
+    @GetMapping("/reviews/product/{productId}")
+    public ResponseEntity<ApiResponse<List<ReviewResponse>>> getByProduct(
+            @PathVariable String productId) {
+        return ResponseEntity.ok(ApiResponse.success(reviewService.getByProduct(productId)));
+    }
+
+
+}
+
