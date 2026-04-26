83337df9a5cf1cc7b597eb1cad6e9368df43f000
Yara <yaradaraghmeh056@gmail.com>
2026-04-26 15:54:36 +0300

    Get Cart Service

---
+++ src/main/java/com/markethive/MarketHive/services/CartService.java
@@ -0,0 +1,11 @@
+package com.markethive.MarketHive.services;
+
+import com.markethive.MarketHive.dto.request.CartRequest;
+import com.markethive.MarketHive.dto.response.CartResponse;
+
+import java.util.List;
+
+public interface CartService {
+    List<CartResponse> getCart(String userId);
+
+}
